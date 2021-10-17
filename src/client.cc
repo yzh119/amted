79642c2f5d2f609375b216e0aaa9fa65bfbc1aaa
Zihao Ye <expye@outlook.com>
2021-10-17 04:42:00 +0000

    upd test

--- src/client.cc
+++ src/client.cc
@@ -1,28 +1,33 @@
+#include <amted/network.h>
+#include <amted/utils.h>
+#include <unistd.h>
+
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#include <unistd.h>
-#include <sys/time.h>
-#include <amted/network.h>
-#include <amted/utils.h>
 
+typedef std::chrono::high_resolution_clock Time;
+typedef std::chrono::milliseconds ms;
+typedef std::chrono::duration<float> fsec;
 
 int download_file(int fd, char *filename, int filename_len) {
   send(fd, filename, filename_len, 0);  // check buffer overflow.
   static char buf[SOCKET_BUFFER_SIZE];
   printf("Sent download request to server...\n");
-  bzero(buf, sizeof(buf)); 
+  bzero(buf, sizeof(buf));
   int ret = recv(fd, buf, sizeof(buf), 0);
   int file_size = std::stoi(buf);
   if (file_size >= 0) {
-    printf("File %s found on server, size %dB...\n", filename, file_size);  
+    printf("File %s found on server, size %dB...\n", filename, file_size);
   } else {
     fprintf(stderr, "Cannot find %s on server...\n", filename);
     return 0;
   }
-  std::string filename_str = std::string(filename); 
-  std::string basename = filename_str.substr(filename_str.find_last_of("/\\") + 1);
+  std::string filename_str = std::string(filename);
+  std::string basename =
+      filename_str.substr(filename_str.find_last_of("/\\") + 1);
   FILE *fp = fopen(basename.c_str(), "w");
   if (fp == NULL) {
     fprintf(stderr, "File %s creation failed...\n", basename.c_str());
@@ -30,7 +35,7 @@ int download_file(int fd, char *filename, int filename_len) {
   }
   int cnt = 0, buf_len = 0;
   bzero(buf, sizeof(buf));
-  while ((buf_len = recv(fd, buf, sizeof(buf), 0)) > 0) {
+  while ((buf_len = recv(fd, buf, std::min<int>(sizeof(buf), file_size - cnt), 0)) > 0) {
     fwrite(buf, sizeof(char), buf_len, fp);
     bzero(buf, sizeof(buf));
     cnt += buf_len;
@@ -42,9 +47,8 @@ int download_file(int fd, char *filename, int filename_len) {
 
 void process_request(char *ip, int port) {
   size_t len = 0;
-  struct timeval current_time;
   int sock_fd;
-  struct sockaddr_in server_addr; 
+  struct sockaddr_in server_addr;
   // create socket
   sock_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sock_fd == 1) {
@@ -57,7 +61,8 @@ void process_request(char *ip, int port) {
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = inet_addr(ip);
   server_addr.sin_port = htons(port);
-  if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+  if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+      0) {
     fprintf(stderr, "Connection with %s failed...\n", ip);
     abort();
   } else {
@@ -68,16 +73,15 @@ void process_request(char *ip, int port) {
   bzero(filename, sizeof(filename));
   while (1) {
     int len = 0;
-    while ((filename[len++] = getchar()) != '\n');
+    while ((filename[len++] = getchar()) != '\n')
+      ;
     filename[len - 1] = '\0';
-    gettimeofday(&current_time, NULL);
-    int start_time = current_time.tv_usec;
     printf("Downloading %s on %s\n", filename, ip);
-    gettimeofday(&current_time, NULL);
+    auto tic = Time::now();
     if (download_file(sock_fd, filename, len)) {
-      gettimeofday(&current_time, NULL);
-      int end_time = current_time.tv_usec;
-      printf("Download successful, total time: %.3fs\n", (end_time - start_time) / 1e6);
+      auto toc = Time::now();
+      printf("Download successful, total time: %lldms\n",
+             std::chrono::duration_cast<ms>(toc - tic).count());
     } else {
       fprintf(stderr, "Download failed...\n");
     }
@@ -86,7 +90,6 @@ void process_request(char *ip, int port) {
   close(sock_fd);
 }
 
-
 int main(int argc, char *argv[]) {
   if (argc == 3) {
     char *ip = NULL;
@@ -94,14 +97,16 @@ int main(int argc, char *argv[]) {
     parse_arguments(argc, argv, &ip, &port);
     process_request(ip, port);
   } else if (argc == 1) {
-    printf("IP address and port must be specified, see %s --help for usage\n", argv[0]);
+    printf("IP address and port must be specified, see %s --help for usage\n",
+           argv[0]);
   } else if (argc == 2 && strcmp(argv[1], "--help") == 0) {
     printf("CSE550 client\n");
     printf("Usage: %s ip_address port\n", argv[0]);
     printf("ip_address  : The IPv4 address of remote host.\n");
     printf("port        : The port to connect to on the remote host.\n");
   } else {
-    fprintf(stderr, "Invalid number of arguments, see %s --help for usage.\n", argv[0]);
+    fprintf(stderr, "Invalid number of arguments, see %s --help for usage.\n",
+            argv[0]);
     abort();
   }
   return 0;
--- tests/gen_file_list.py
+++ tests/gen_file_list.py
@@ -1,4 +1,4 @@
 import random
 
-for i in range(300):
+for i in range(10000):
     print(str(random.randint(0, 1000)) + '.html')
